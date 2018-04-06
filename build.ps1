9b5ca574d42c85fa329ab4228b8a5270bbfa76ad
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-04-06 12:56:33 +0200

    remove old toolchain chainload

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@
 mkdir -Force build
 cd build
 
-cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=$env:WORKSPACE\sysconfig\cmake\physycom_toolchain.cmake" "-DCMAKE_BUILD_TYPE=Release" ..
+cmake -G "Ninja" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Release" ..
 cmake --build . --config Release
 
 cd ..
